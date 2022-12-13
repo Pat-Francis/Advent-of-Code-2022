173be79ccfcd6fc04ab2909a4a43fd9ab30ba356
Pat-Francis <github@pmfrancis.com>
2022-12-13 20:38:09 +0000

    Day 3 Part 2 completed

--- Day-03/main.py
+++ Day-03/main.py
@@ -1,14 +1,22 @@
 def process_input(file: str):
     with open(file) as f:
         rucksacks = f.read().split("\n")
-        split_sacks = [[i[:len(i)//2], i[len(i)//2:]] for i in rucksacks]
 
-        return split_sacks
+        return rucksacks
+
+
+def split_sacks(rucksacks):
+    return [[i[:len(i)//2], i[len(i)//2:]] for i in rucksacks]
+
+
+def grouped_elves(rucksacks):
+    return [rucksacks[x:x + 3] for x in range(0, len(rucksacks), 3)]
 
 
 def part_one(filename: str):
     priority_sum = 0
-    rucks = process_input(filename)
+    rucksacks = process_input(filename)
+    rucks = split_sacks(rucksacks)
 
     for ruck in rucks:
         common_item = list(set(ruck[0]) & set(ruck[1]))
@@ -22,10 +30,24 @@ def part_one(filename: str):
 
 
 def part_two(filename: str):
-    pass
+    rucksacks = process_input(filename)
+    groups = grouped_elves(rucksacks)
+    priority_sum = 0
+
+    for group in groups:
+        badge = list(set(group[0]) & set(group[1]) & set(group[2]))
+
+        if badge[0].isupper():
+            priority_sum += ord(badge[0]) - ord('A') + 27
+        else:
+            priority_sum += ord(badge[0]) - ord('a') + 1
+
+    return priority_sum
 
 
 if __name__ == "__main__":
     input_file = "./input.txt"
     print(part_one(input_file))
     print(part_two(input_file))
+
+# TODO: Refactor priority_sum into separate function
