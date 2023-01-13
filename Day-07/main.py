a74e0f56ceea220a8b38a29cda4b00c3e27e1d73
Pat-Francis <programming@pmfrancis.com>
2023-01-13 20:51:33 +0000

    Part 1 Completed

--- Day-07/main.py
+++ Day-07/main.py
@@ -2,7 +2,7 @@ def process_input(file: str):
     with open(file) as f:
         input_data = f.read().splitlines()
     current_dir = ""
-    dir_structure = {"/": {"child_dir": [], "files": {}}}
+    dir_structure = {"/": {"parent_dir": "/", "child_dir": [], "files": {}}}
 
     for line in input_data:
         parts = line.split(" ")
@@ -11,18 +11,18 @@ def process_input(file: str):
                 pass
             else:
                 if parts[2] == "..":
-                    current_dir = current_dir[:-1]
+                    current_dir = dir_structure[current_dir].get("parent_dir")
                 else:
+                    parent_dir = current_dir
                     current_dir += parts[2]
+
                     if current_dir not in dir_structure:
-                        dir_structure.update({current_dir: {"child_dir": [], "files": {}}})
+                        dir_structure.update({current_dir: {"parent_dir": parent_dir, "child_dir": [], "files": {}}})
+
         elif parts[0] == "dir":
             dir_structure[current_dir]["child_dir"].append(f"{current_dir}{parts[1]}")
         else:
             dir_structure[current_dir]["files"].update({parts[1]: int(parts[0])})
-
-    # for k, v in dir_structure.items():
-    #     print(k, v)
 
     return dir_structure
 
@@ -49,8 +49,6 @@ def part_one(filename: str):
 
         if total_size < 100000:
             dir_sizes[k] = total_size
-    # print("---------")
-    # print(dir_sizes)
 
     return sum(dir_sizes.values())
 
@@ -60,6 +58,6 @@ def part_two(filename: str):
 
 
 if __name__ == "__main__":
-    input_file = "./test_input.txt"
+    input_file = "./input.txt"
     print(part_one(input_file))
     print(part_two(input_file))
