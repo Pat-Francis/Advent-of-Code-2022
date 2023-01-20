fa913afdf466880e26ed7e8521d49224533299a9
Pat-Francis <programming@pmfrancis.com>
2023-01-20 17:23:02 +0000

    Day 8 Completed

--- Day-08/main.py
+++ Day-08/main.py
@@ -10,6 +10,24 @@ def process_input(file: str):
     return tree_grid
 
 
+def scenic_score(views: list[list], tree_height: int):
+    view_score = 1
+
+    for direction in views:
+        dir_score = 0
+
+        for tree in direction:
+            if tree < tree_height:
+                dir_score += 1
+            else:
+                dir_score += 1
+                break
+
+        view_score *= dir_score
+
+    return view_score
+
+
 def part_one(filename: str):
     tree_grid = process_input(filename)
     visible_trees = (len(tree_grid[0]) * 2) + (len(tree_grid) - 2) * 2  # Counts all the 'edge' trees
@@ -18,10 +36,10 @@ def part_one(filename: str):
     for i, row in enumerate(tree_grid[1:-1]):
         for j, tree in enumerate(row[1:-1]):
             adj_check = (
-                    max([row[j + 1] for row in tree_grid][:i + 1]) < tree_grid[i + 1][j + 1],  # North
-                    max([row[j + 1] for row in tree_grid][i + 2:]) < tree_grid[i + 1][j + 1],  # South
-                    max(row[j + 2:]) < tree_grid[i + 1][j + 1],  # East
-                    max(row[:j + 1]) < tree_grid[i + 1][j + 1],  # West
+                max([row[j + 1] for row in tree_grid][:i + 1]) < tree_grid[i + 1][j + 1],  # North
+                max([row[j + 1] for row in tree_grid][i + 2:]) < tree_grid[i + 1][j + 1],  # South
+                max(row[j + 2:]) < tree_grid[i + 1][j + 1],  # East
+                max(row[:j + 1]) < tree_grid[i + 1][j + 1],  # West
             )
             if any(adj_check):
                 visible_trees += 1
@@ -30,7 +48,18 @@ def part_one(filename: str):
 
 
 def part_two(filename: str):
-    pass
+    tree_grid = process_input(filename)
+    view_scores = []
+
+    for i, row in enumerate(tree_grid[1:-1]):
+        for j, tree in enumerate(row[1:-1]):
+            views = [[row[j + 1] for row in tree_grid][i::-1],  # North
+                     [row[j + 1] for row in tree_grid][i + 2:],  # South
+                     row[j + 2:],  # East
+                     row[j::-1]]  # West
+            view_scores.append(scenic_score(views, tree_grid[i + 1][j + 1]))
+
+    return max(view_scores)
 
 
 if __name__ == "__main__":
