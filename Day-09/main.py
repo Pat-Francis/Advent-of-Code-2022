d2b2380fd86346c2ca7795035731172979baa152
Pat-Francis <programming@pmfrancis.com>
2023-01-22 17:43:53 +0000

    Day 9 Refactor

--- Day-09/main.py
+++ Day-09/main.py
@@ -11,39 +11,8 @@ def process_input(file: str):
     return head_moves
 
 
-def part_one(filename: str):
-    moves = process_input(filename)
-    head_loc = [0, 0]
-    tail_loc = [0, 0]
-    tail_trail = []
-    head_deltas = {'R': [0, 1],
-                   'L': [0, -1],
-                   'U': [1, 0],
-                   'D': [-1, 0]
-                   }
-
-    for move in moves:
-        direction = move[0]
-        steps = move[1]
-
-        for step in range(0, steps):
-            prev_head = head_loc
-            head_loc = [sum(i) for i in zip(head_loc, head_deltas.get(direction))]
-            head_tail_delta = [x - y for (x, y) in zip(head_loc, tail_loc)]
-
-            if any(i > 1 for i in head_tail_delta) or any(i < -1 for i in head_tail_delta):
-                tail_loc = prev_head
-
-            if tail_loc not in tail_trail:
-                tail_trail.append(tail_loc)
-
-    return len(tail_trail)
-
-
-def part_two(filename: str):
-    moves = process_input(filename)
-    knots_length = 10
-    knots = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+def simulate_rope(moves, knots_length):
+    knots = [[0, 0]] * knots_length
     knots_indices = [[i, i + 1] for i in range(0, knots_length - 1)]
     tail_trail = []
     head_deltas = {'R': [1, 0],
@@ -64,10 +33,12 @@ def part_two(filename: str):
                 head_tail_delta = [x - y for (x, y) in zip(knots[head], knots[tail])]
 
                 tail_update = []
+                # Diagonal Tail update
                 if sum(abs(i) for i in head_tail_delta) > 2:
                     for coord in head_tail_delta:
                         tail_update.append(coord // abs(coord))
                 else:
+                    # Up, Down, Left, Right Tail update
                     for coord in head_tail_delta:
                         if coord > 1:
                             tail_update.append(1)
@@ -78,10 +49,24 @@ def part_two(filename: str):
 
                 knots[tail] = [sum(i) for i in zip(knots[tail], tail_update)]
 
-                if tail == 9 and knots[tail] not in tail_trail:
+                if tail == knots_length - 1 and knots[tail] not in tail_trail:
                     tail_trail.append(knots[tail])
 
     return len(tail_trail)
+
+
+def part_one(filename: str):
+    moves = process_input(filename)
+    knots_length = 2
+
+    return simulate_rope(moves, knots_length)
+
+
+def part_two(filename: str):
+    moves = process_input(filename)
+    knots_length = 10
+
+    return simulate_rope(moves, knots_length)
 
 
 if __name__ == "__main__":
