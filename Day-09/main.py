0791c05d2e861eeb3cfc0814090a9218f9fea5ab
Pat-Francis <programming@pmfrancis.com>
2023-01-22 17:17:26 +0000

    Day 9 Completed

--- Day-09/main.py
+++ Day-09/main.py
@@ -41,7 +41,47 @@ def part_one(filename: str):
 
 
 def part_two(filename: str):
-    pass
+    moves = process_input(filename)
+    knots_length = 10
+    knots = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    knots_indices = [[i, i + 1] for i in range(0, knots_length - 1)]
+    tail_trail = []
+    head_deltas = {'R': [1, 0],
+                   'L': [-1, 0],
+                   'U': [0, 1],
+                   'D': [0, -1]
+                   }
+
+    for move in moves:
+        direction = move[0]
+        steps = move[1]
+
+        for step in range(0, steps):
+            for head, tail in knots_indices:
+                if head == 0:
+                    knots[head] = [sum(i) for i in zip(knots[head], head_deltas.get(direction))]
+
+                head_tail_delta = [x - y for (x, y) in zip(knots[head], knots[tail])]
+
+                tail_update = []
+                if sum(abs(i) for i in head_tail_delta) > 2:
+                    for coord in head_tail_delta:
+                        tail_update.append(coord // abs(coord))
+                else:
+                    for coord in head_tail_delta:
+                        if coord > 1:
+                            tail_update.append(1)
+                        elif coord < -1:
+                            tail_update.append(-1)
+                        else:
+                            tail_update.append(0)
+
+                knots[tail] = [sum(i) for i in zip(knots[tail], tail_update)]
+
+                if tail == 9 and knots[tail] not in tail_trail:
+                    tail_trail.append(knots[tail])
+
+    return len(tail_trail)
 
 
 if __name__ == "__main__":
